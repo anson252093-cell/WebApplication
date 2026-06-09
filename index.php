4cf2207bc0e2bedd35586d6fa377ad0400cb2eb4
Anson Lim <anson252093@dasein.edu.my>
2026-06-09 10:39:09 +0800

    index testing

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+$servername = "localhost";
+$username = "anson";
+$password = "ALZK0705";
+$dbname = "anson";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <style>
+    * {
+      font-size: 20px;
+    }
+
+    body {display: flex; 
+          justify-content: center; 
+          align-items: center; 
+          height: 100vh;
+        }
+  </style>
+</head>
+<body>
+  <div id="email">
+    <form target="_self" method="POST">
+      <h2>Enter your Email</h2>
+      <input type="text" name="email">
+      <br/>
+      <h2>Password</h2>
+      <input type="password" name="password">
+      <input type="submit">
+    </form>
+  </div>
+</body>
+</html>
